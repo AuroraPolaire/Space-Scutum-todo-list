572a63e89c8e5afe6d8d138ee472c0f5237ab493
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-11-30 12:35:23 +0200

    feature/pagination/moved logic to usePagination hook

--- src/App.js
+++ src/App.js
@@ -1,36 +1,23 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Pagination from '@mui/material/Pagination';
 import { getAllTODOs, getTODOs } from './redux/todoOperations';
 import ToDoList from './components/ToDoList/ToDoList';
 import NewTodo from './components/NewTodo/NewTodo';
-import { selectPageNumber, selectTotalAmount } from './redux/todoSelector';
 import { StyledApp } from './App.styled';
 import { GlobalContainer } from './GlobalContainer/GlobalContainer';
 import Header from './components/Header/Header';
-import { setPageNumber } from './redux/todoSlice';
+import usePagination from './hooks/usePagination';
 
 function App() {
   const dispatch = useDispatch();
-  const totalTodos = useSelector(selectTotalAmount);
-  const rememberedPageNumber = useSelector(selectPageNumber);
 
-  const [totalPages, setTotalPages] = useState(Math.ceil(totalTodos / 5));
-  const [pageNumber, setPage] = useState(rememberedPageNumber);
-
-  const handlePageChange = (event, value) => {
-    dispatch(setPageNumber(value));
-    setPage(value);
-  };
+  const { pageNumber, handlePageChange, totalPages } = usePagination();
 
   useEffect(() => {
     dispatch(getAllTODOs());
     dispatch(getTODOs(pageNumber));
   }, [dispatch, pageNumber]);
-
-  useEffect(() => {
-    setTotalPages(Math.ceil(totalTodos / 6));
-  }, [totalTodos]);
 
   return (
     <>
---
+++ src/hooks/usePagination.jsx
@@ -0,0 +1,26 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectPageNumber, selectTotalAmount } from '../redux/todoSelector';
+import { setPageNumber } from '../redux/todoSlice';
+import { useEffect, useState } from 'react';
+
+const usePagination = () => {
+  const dispatch = useDispatch();
+  const totalTodos = useSelector(selectTotalAmount);
+  const rememberedPageNumber = useSelector(selectPageNumber);
+
+  const [totalPages, setTotalPages] = useState(Math.ceil(totalTodos / 5));
+  const [pageNumber, setPage] = useState(rememberedPageNumber);
+
+  useEffect(() => {
+    setTotalPages(Math.ceil(totalTodos / 6));
+  }, [totalTodos]);
+
+  const handlePageChange = (event, value) => {
+    dispatch(setPageNumber(value));
+    setPage(value);
+  };
+
+  return { pageNumber, handlePageChange, totalPages };
+};
+
+export default usePagination;
